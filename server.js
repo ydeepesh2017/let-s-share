bc11a680e2d542556dc09f2badd285ebfdbb2b43
DEEPESH YADAV <72725770+ydeepesh2017@users.noreply.github.com>
2021-09-15 13:24:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+app.use(cors(corsOptions))
+
+
 // Default configuration looks like
 // {
 //     "origin": "*",
