c7b1416741224043a8832e0cc03abda0ff82166f
DEEPESH YADAV <72725770+ydeepesh2017@users.noreply.github.com>
2021-09-15 13:39:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 // Cors 
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  ['https://lets-share001.herokuapp.com']
 }
 app.use(cors(corsOptions))
 
